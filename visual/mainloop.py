3a71cc917c1594d6cd230ccfbbbed3ea36492a62
Grepleon <kharit.artem@gmail.com>
2026-07-24 18:11:22 +0300

    create base for display in a window

--- visual/mainloop.py
+++ visual/mainloop.py
@@ -8,6 +8,39 @@ class Display:
         self.canvas = Canvas(self.root, width=x, height=y, bg=config.bg_color)
         self.canvas.grid()
         self.canvas.pack(anchor=CENTER, expand=1)
+        self.id_objects = 1
 
     def end(self):
         self.root.mainloop()
+
+    def create_rectangle(self, x1, y1, x2, y2, color, outline, tag=None) -> int | str:
+        if tag is None:
+            self.id_objects += 1
+            self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline=outline, tags=str(self.id_objects))
+            return self.id_objects
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline=outline, tags=tag)
+        return tag
+
+    def create_circle(self, x1, y1, x2, y2, color, outline, tag=None) -> int | str:
+        if tag is None:
+            self.id_objects += 1
+            self.canvas.create_oval(x1, y1, x2, y2, fill=color, outline=outline, tags=str(self.id_objects))
+            return self.id_objects
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline=outline, tags=tag)
+        return tag
+
+    def create_line(self, x1, y1, x2, y2, color, tag=None) -> int | str:
+        if tag is None:
+            self.id_objects += 1
+            self.canvas.create_line(x1, y1, x2, y2, fill=color, tags=str(self.id_objects))
+            return self.id_objects
+        self.canvas.create_line(x1, y1, x2, y2, fill=color, tags=tag)
+        return tag
+
+    def create_text(self, x1, y1, text, color, tag=None) -> int | str:
+        if tag is None:
+            self.id_objects += 1
+            self.canvas.create_text(x1, y1, text=text, fill=color, tags=str(self.id_objects))
+            return self.id_objects
+        self.canvas.create_text(x1, y1, text=text, fill=color, tags=tag)
+        return tag
