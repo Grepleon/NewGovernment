70d4c0058d3c2345f8cc73d245db2a7af788cbeb
Grepleon <grepleon.dev@gmail.com>
2026-09-05 11:42:00 +0300

    add ideas to parties

--- party/base_party.py
+++ party/base_party.py
@@ -1,7 +1,10 @@
 import politicians.politician as pol
+import politicians.policy.political_compass as pol_com
 
 class BaseParty:
-    def __init__(self, name, leader: pol.Politician, participants: list[pol.Politician]):
+    def __init__(self, name, leader: pol.Politician, participants: list[pol.Politician],
+                 political_ideas:pol_com.PoliticalCompass):
         self.name:str = name
         self.leader: pol.Politician = leader
         self.participants: list[pol.Politician] = participants
+        self.political_ideas = political_ideas
