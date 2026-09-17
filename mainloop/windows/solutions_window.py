91aced3e11e2e52788232ec99d59ba9b39fac2d4
Grepleon <grepleon.dev@gmail.com>
2026-09-17 23:56:40 +0300

    solutions

--- mainloop/windows/solutions_window.py
+++ mainloop/windows/solutions_window.py
@@ -4,40 +4,48 @@ from visual.components.hint import Hint
 from visual.display import Display
 from visual.components.base_object import BaseObject
 from visual.components.button import Button
-
-def space(text:str, max_char:int, extra_max:int):
-    count_char=0
-    new_text = ""
-    for char in text:
-        count_char += 1
-        if char == "\n":
-            count_char = 0
-
-        if char != " ":
-            if count_char < extra_max:
-                new_text += char
-            else:
-                new_text += char + "-\n"
-                count_char = 0
-        elif count_char > max_char:
-            new_text += "\n"
-            count_char = 0
-        else:
-            new_text += " "
-
-    if new_text[-1] == "\n":
-        new_text = new_text[:-1]
-    if new_text[-1] == "-":
-        new_text = new_text[:-1]
-
-    return new_text.replace("-\n ", "\n")
+from hints.enters import space
+from politicians.politician import Politician
 
 class SolutionWindow(AdditionalWindow):
     hint:Hint|None = None
+    last_button:Button|None = None
+
+    def get_sum_polh(self):
+        sum_polh = 0
+        for component in self.components:
+            if type(component) == Button and component != self.last_button:
+                if component.on:
+                    sum_polh += component.info.get("polh")
+        return sum_polh
+
+    def check_last_button(self):
+        selected_politician: Politician = self.game_state.selected_politician
+
+        if self.last_button.into_mouse:
+            self.hint.show()
+            self.hint.rewrite_text(space(
+                f"Будет затрачено {self.get_sum_polh()} полит. часов.\n"
+                f"Вам {f"не хватает {self.get_sum_polh() - selected_politician.political_hour 
+                }" if selected_politician.political_hour < self.get_sum_polh() else
+                "хватает"} полит. часов.",
+                20, 25))
+            self.hint.to_move(
+                self.display.mouse_x,
+                self.display.mouse_y
+            )
+        if self.last_button.on:
+            if selected_politician.political_hour >= self.get_sum_polh():
+                selected_politician.political_hour -= self.get_sum_polh()
+
+            self.display.switch_window(self.display.main_window)
+            self.display.destroy_window(self.name_window)
+            return True
+        return False
 
     def process(self):
-        self.display.switch_window(self.name_window)
         if self.check_activity():
+            self.display.switch_window(self.name_window)
             self.hint.hide()
 
             for component in self.components:
@@ -50,5 +58,7 @@ class SolutionWindow(AdditionalWindow):
                             self.display.mouse_x,
                             self.display.mouse_y
                         )
+            if self.check_last_button():
+                return
 
         self.display.switch_window(self.display.main_window)
