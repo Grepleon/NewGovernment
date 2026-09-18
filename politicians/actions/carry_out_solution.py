5600244853843650587370b77b470c7408515a06
Grepleon <grepleon.dev@gmail.com>
2026-09-18 18:56:44 +0300

    first solutions

--- politicians/actions/carry_out_solution.py
+++ politicians/actions/carry_out_solution.py
@@ -5,7 +5,5 @@ dict_solutions = {
 }
 
 def carry_out_solutions(name_sol, display, game_states, politician):
-    print(name_sol)
     if name_sol in dict_solutions:
-        print('x')
         dict_solutions[name_sol](politician, display, game_states)
--- politicians/actions/solutions/make_a_declaration.py
+++ politicians/actions/solutions/make_a_declaration.py
@@ -6,4 +6,3 @@ from random import randint
 def carry_out(politician:Politician, display:Display, game_states:GameState):
     pop_factor = politician.get_populist_factors()
     politician.popularity.for_peoples(int(pop_factor / 10 * randint(3, 4)) + 1)
-    print('популярка', politician.popularity.to_str())
